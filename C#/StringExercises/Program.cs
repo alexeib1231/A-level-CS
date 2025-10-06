2cc505e6c0d1eeb113890ca29c0fc7a88b791735
alexeib1231 <abogosla2008@gmail.com>
2025-10-06 19:47:19 +0100

    Update Program.cs

--- C#/StringExercises/Program.cs
+++ C#/StringExercises/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 xdads
+﻿List<string>  Wave(string str)
+{
+    List<string> waved = new List<string>();
+    for (int i = 0; i < str.Length; i++)
+    {
+        str.ToLower();
+        str[i].ToUpper();
+        waved.Add(str);
+    }
+    return waved;
+}
+Console.WriteLine(Wave("hello"));
