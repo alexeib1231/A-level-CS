62ab7d16104150597c67d73653dd4a9fa8292248
alexeib1231 <abogosla2008@gmail.com>
2025-10-15 18:41:53 +0100

    bitwise cont.

--- C#/BitwiseExerices/BitwiseExerices/Program.cs
+++ C#/BitwiseExerices/BitwiseExerices/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 UInt32 rightmostbit(UInt32 n)
 {
@@ -24,3 +23,27 @@ UInt32 threeleftmostbits(UInt32 n)
     return (n >> 29) & 0b111;
 }
 Debug.Assert(threeleftmostbits(uint.MaxValue) == 111);
+
+int removerightmostbit(int n)
+{
+    return n & ~1;
+}
+Debug.Assert(removerightmostbit(0b11) == 0b10);
+
+int removethreerightmostbits(int n)
+{
+    return n & ~0b111;
+}
+Debug.Assert(removethreerightmostbits(0b1111) == 0b1000);
+
+int removeleftmostbit(int n)
+{
+    return n & ~(1 << 31);
+}
+Debug.Assert(removeleftmostbit(int.MinValue) == 0);
+
+int removethreeleftmostbits(int n)
+{
+    return n & ~(0b111 << 29);
+}
+Debug.Assert(removethreeleftmostbits(int.MinValue) == 0b001111);
